53da5e914e7a786a0a994b6bcf8ef3a42cddd834
SAULO SANTOS <BA9132@sescbahia.com.br>
2021-10-05 15:02:04 -0300

    Adicionar arquivos de projeto.

---
+++ Estudo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Estudo.MvcApplication" Language="C#" %>
---
+++ Estudo/Models/DBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Estudo.Models
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
